8c88d8b5ecff08398a696a04c55fdb1883a12638
Felipe Nunes <febatera.soares@gmail.com>
2020-06-25 19:30:46 -0300

    Back-end do curso Semana Omnistack 11
    Todo o back-end da aplicação, feita em Javascript, através do Node.js, utilizando o banco de dados SQLite.

---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const Ongs = require('./controllers/Ongs')
+const Incidents = require('./controllers/Incidents')
+const Profile = require('./controllers/Profile')
+const Session = require('./controllers/Session')
+
+const routes = express.Router()
+
+routes.get('/ongs', Ongs.index)
+routes.post('/ongs', Ongs.create)
+
+routes.get('/incidents', Incidents.index)
+routes.post('/incidents', Incidents.create)
+routes.put('/incidents', Incidents.update)
+routes.delete('/incidents/:id', Incidents.delete)
+
+routes.get('/profile', Profile.index)
+
+routes.post('/session', Session.create)
+
+module.exports = routes
